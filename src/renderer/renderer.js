59af23f2a1451f73f36ae35dd2a72d070c2e1e98
Asier Turrillo <asier.turrillo@ingenoba.com>
2024-04-29 15:48:57 +0200

    Simplyfied app

--- src/renderer/renderer.js
+++ src/renderer/renderer.js
@@ -18,113 +18,47 @@ function startCountdown() {
     }
 
     interval = setInterval(() => {
-        if (parseInt(timer.textContent) > 0) {
-            timer.textContent = String(parseInt(timer.textContent) - 1);
+        if (timer.value > 0) {
+            timer.value = timer.value - 1;
         } else {
             clearInterval(interval);
-            timer.style.color = "#7077A1";
-            timeControl.textContent == "Start"
-            timeInput.classList.remove('hidden');
-            menuContainer.classList.remove('hidden');
-            container.classList.remove('counter-expanded');
-            container.classList.add('counter');
+            timeControl.textContent = "Start";
         }
     }, 1000);
 }
-
-forward.addEventListener('click', () => {
-    timer.textContent = String(parseInt(timer.textContent) - 30);
-});
-
-backward.addEventListener('click', () => {
-    timer.textContent = String(parseInt(timer.textContent) + 30);
-});
-
-timeControl.addEventListener('click', () => {
-    const action = timeControl.textContent;
-
-    switch (action) {
-        case "Start":
-            timeControl.textContent = "Pause";
-
-            if (timeInput.value == null || timeInput.value == "") {
-                timeInput.value = 0;
-            }
-
-            timer.style.color = "#F6B17A";
-            timeInput.classList.add('hidden');
-            menuContainer.classList.add('hidden');
-            container.classList.add('counter-expanded');
-            container.classList.remove('counter');
-
-            timer.textContent = String(timeInput.value);
-            startCountdown();
-            break;
-        case "Continue":
-            timeControl.textContent = "Pause";
-
-            if (timer.textContent == null || timer.textContent == "") {
-                timer.textContent = "0";
-            }
-
-            timeInput.classList.add('hidden');
-            menuContainer.classList.add('hidden');
-            container.classList.add('counter-expanded');
-            container.classList.remove('counter');
-
-            startCountdown();
-            timer.style.color = "#F6B17A";
-            break;
-        case "Pause":
-            timeControl.textContent = "Continue";
-            timer.style.color = "#7077A1";
-            clearInterval(interval);
-            break;
-    }
-});
-
-timeControl.addEventListener('dblclick', () => {
-    timeControl.textContent = "Start";
-    timer.style.color = "#7077A1";
-    clearInterval(interval);
-});
-
-timeInput.addEventListener('change', () => {
-    if (timeInput.value == null || timeInput.value == "") {
-        timeInput.value = 0;
-    }
-});
 
 closeButton.addEventListener('click', () => {
     appControl.closeApp();
 });
 
-timerContainer.addEventListener('dblclick', () => { 
-    timeInput.classList.remove('hidden');
-    menuContainer.classList.remove('hidden');
-    container.classList.remove('counter-expanded');
-    container.classList.add('counter');
+timeControl.addEventListener('click', () => {
+    if (timeControl.textContent == "Start") {
+        timeControl.textContent = "Pause";
+        startCountdown();
+    } else {
+        timeControl.textContent = "Start";
+        clearInterval(interval);
+    }
 });
 
-timerContainer.addEventListener('click', () => { 
-    const action = timeControl.textContent;
+backward.addEventListener('click', () => {
+    timer.value = parseInt(timer.value) - 15;
+});
 
-    switch (action) {
-        case "Continue":
-            timeControl.textContent = "Pause";
+forward.addEventListener('click', () => {
+    timer.value = parseInt(timer.value) + 15;
+});
 
-            timeInput.classList.add('hidden');
-            menuContainer.classList.add('hidden');
-            container.classList.add('counter-expanded');
-            container.classList.remove('counter');
+timer.addEventListener('dblclick', () => {
+    if (container.classList.contains('counter-expanded')) {
+        container.classList.remove('counter-expanded');
+        container.classList.add('counter');
 
-            startCountdown();
-            timer.style.color = "#F6B17A";
-            break;
-        case "Pause":
-            timeControl.textContent = "Continue";
-            timer.style.color = "#7077A1";
-            clearInterval(interval);
-            break;
+        menuContainer.classList.remove('hidden');
+    } else {
+        container.classList.add('counter-expanded');
+        container.classList.remove('counter');
+
+        menuContainer.classList.add('hidden');
     }
 });
